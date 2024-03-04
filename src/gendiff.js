f41dadfbd1c49ef024ac7f4f7c321c3bef4c6871
zzpillau <zzpilla@gmail.com>
2024-03-04 16:40:55 +0200

    update gendiff

--- src/gendiff.js
+++ src/gendiff.js
@@ -4,7 +4,10 @@ import { program } from 'commander'
 
 program
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('filepath1 filepath2')
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help', 'output usage information')
   .parse(process.argv)
 
 
