049c66e7c2bde7e38f2ed74b89a2846125441577
zzpillau <zzpilla@gmail.com>
2024-05-01 16:27:24 +0200

    update formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,17 +2,17 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
+const formatter = {
+  stylish: (diffTree) => stylish(diffTree),
+  plain: (diffTree) => plain(diffTree),
+  json: (diffTree) => json(diffTree),
+};
+
 const selectFormatter = (formatterName, diffTree) => {
-  switch (formatterName) {
-    case 'stylish':
-      return stylish(diffTree);
-    case 'plain':
-      return plain(diffTree);
-    case 'json':
-      return json(diffTree);
-    default:
-      throw new Error(`Unknown ${formatterName}`);
+  if (formatter[formatterName]) {
+    return formatter[formatterName](diffTree);
   }
+  throw new Error(`Unknown ${formatterName}`);
 };
 
 export default selectFormatter;
